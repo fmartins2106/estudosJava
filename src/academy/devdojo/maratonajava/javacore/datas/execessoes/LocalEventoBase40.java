fbf53dc9b1a5ef5c528f2fda07874fa34f7c9bc8
Fernando Martins <fernandom.adm@gmail.com>
2025-05-06 21:26:57 -0300

    Criação exercicio40 AgendaTest

---
+++ src/academy/devdojo/maratonajava/javacore/datas/execessoes/LocalEventoBase40.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.datas.execessoes;
+
+public class LocalEventoBase40 extends IllegalArgumentException {
+    public LocalEventoBase40() {
+        super("Campo nome não pode ser vazio ou conter caracteres.");
+    }
+}
