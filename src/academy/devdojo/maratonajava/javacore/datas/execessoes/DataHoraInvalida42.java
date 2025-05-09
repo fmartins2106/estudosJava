65df5de1213f3e2d1a7bf3b93b949edf44ab883a
Fernando Martins <fernandom.adm@gmail.com>
2025-05-09 07:56:53 -0300

    Criação exercicio EventoTest42

---
+++ src/academy/devdojo/maratonajava/javacore/datas/execessoes/DataHoraInvalida42.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.datas.execessoes;
+
+public class DataHoraInvalida42 extends IllegalArgumentException {
+    public DataHoraInvalida42() {
+        super("Data ou hora inválida.");
+    }
+}
