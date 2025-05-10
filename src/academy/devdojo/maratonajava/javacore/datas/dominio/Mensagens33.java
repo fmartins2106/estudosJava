a3d11752a9fc400590a7a4f4542f729c1f492e90
Fernando Martins <fernandom.adm@gmail.com>
2025-05-10 15:11:24 -0300

    Criação exercicio SistemaFaturas33

---
+++ src/academy/devdojo/maratonajava/javacore/datas/dominio/Mensagens33.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.datas.dominio;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Mensagens33 {
+    public static ResourceBundle carregarIdiomas(String idioma){
+        Locale locale = idioma.equalsIgnoreCase("en") ? new Locale("en","US") :
+                new Locale("pt","BR");
+        return ResourceBundle.getBundle("mensagensBundle.messages33",locale);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/datas/execessoes/DataVencimentoFaturaException33.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.datas.execessoes;
+
+public class DataVencimentoFaturaException33 extends IllegalArgumentException {
+    public DataVencimentoFaturaException33() {
+        super("Data de vencimento inválida. Digite uma data no formato DD/MM/AAAA");
+    }
+}
