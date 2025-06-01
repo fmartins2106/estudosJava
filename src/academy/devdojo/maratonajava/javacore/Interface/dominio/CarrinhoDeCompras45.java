00627316c6f5f80b82d8f806b8ef284dcf1a5f17
Fernando Martins <fernandom.adm@gmail.com>
2025-06-01 18:00:14 -0300

    Treinando Map

--- src/academy/devdojo/maratonajava/javacore/Interface/dominio/CarrinhoDeCompras45.java
+++ src/academy/devdojo/maratonajava/javacore/Interface/dominio/CarrinhoDeCompras45.java
@@ -16,6 +16,7 @@ public class CarrinhoDeCompras45 {
         }
     }
 
+    
     public void retirarProdutoCarrinho(int index){
         List<Produto45> produtos = new ArrayList<>(carrinhoDeCompras.keySet());
         Produto45 produtoSelecionado = produtos.get(index);
