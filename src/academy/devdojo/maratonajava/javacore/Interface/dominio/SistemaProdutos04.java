fa202e1e8b2e57a17fed5fd8d1d14ccf38efea80
Fernando Martins <fernandom.adm@gmail.com>
2025-06-05 09:50:02 -0300

    Treinando Map

--- src/academy/devdojo/maratonajava/javacore/Interface/dominio/SistemaProdutos04.java
+++ src/academy/devdojo/maratonajava/javacore/Interface/dominio/SistemaProdutos04.java
@@ -123,12 +123,5 @@ public class SistemaProdutos04 {
         return dadosProdutos.values().stream()
                 .mapToDouble(p -> p.getPreco() * p.getQuantidade()).sum();
     }
-
-
-
-
-
-
-
-
+    
 }
