d3d47c9a1e2f60db1ad42468d2c9c109ab7ce0e7
Fernando Martins <fernandom.adm@gmail.com>
2025-06-12 23:10:42 -0300

    Treinando Logger

--- src/academy/devdojo/maratonajava/javacore/navigableMap/dominio/ConsultaLogger01.java
+++ src/academy/devdojo/maratonajava/javacore/navigableMap/dominio/ConsultaLogger01.java
@@ -44,5 +44,36 @@ public class ConsultaLogger01 {
         }
     }
 
+    private static final String LOGS_DIRETORIO = "logs";
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
+
+//    private static Logger getLogger(Class<?> clazz){
+//        Logger logger = Logger.getLogger(clazz.getName());
+//        logger.setUseParentHandlers(false);
+//
+//        if (logger.getHandlers().length == 0){
+//            configurarLogger2(logger,clazz.getSimpleName());
+//        }
+//        return logger;
+//    }
+//
+//    private static void configurarLogger2(Logger logger, String className){
+//        try {
+//            Files.createDirectories(Paths.get(LOGS_DIRETORIO));
+//            String dataAtual = DATE_FORMAT.format(new Date());
+//            String logFileName = String.format("log_%s_%s.logs",className,dataAtual);
+//            Path logFilePath = Paths.get(LOGS_DIRETORIO,logFileName);
+//
+//            FileHandler fileHandler = new FileHandler(logFilePath.toString(),true);
+//            fileHandler.setFormatter(new SimpleFormatter());
+//            fileHandler.setLevel(Level.ALL);
+//
+//            logger.addHandler(fileHandler);
+//            logger.setLevel(Level.ALL);
+//        }catch (IOException e){
+//            System.out.println("Erro na configuração do logger ->"+className+" :"+e.getMessage());
+//        }
+//    }
+
 
 }
