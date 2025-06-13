8055c7c0d69a7ec703f16e64a81c35fd112a4b4d
Fernando Martins <fernandom.adm@gmail.com>
2025-06-12 23:57:56 -0300

    Treinando exportação de relatório csv

--- src/academy/devdojo/maratonajava/javacore/Interface/dominio/RelatorioEstoque34.java
+++ src/academy/devdojo/maratonajava/javacore/Interface/dominio/RelatorioEstoque34.java
@@ -1,5 +1,6 @@
 package academy.devdojo.maratonajava.javacore.Interface.dominio;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -45,6 +46,42 @@ public class RelatorioEstoque34 {
             e.printStackTrace();
         }
     }
+
+//    private static void gerarRelatorio(List<ProdutoBase34>produtoBase34s){
+//        Path pastaArquivo = Paths.get("relatorio");
+//        try {
+//            if (Files.notExists(pastaArquivo)){
+//                Files.createDirectories(pastaArquivo);
+//            }
+//            Path caminhoArquivo = pastaArquivo.resolve("relatorio_estoque.csv");
+//            StringBuilder conteudo = new StringBuilder();
+//            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//
+//            conteudo.append("\"nome\",\"Preço\",\"Quantidade\",\"Estoque mínimo\",\"Extra\"\n");
+//            String extra = "";
+//            for (ProdutoBase34 produtoBase34 : produtoBase34s) {
+//                if (produtoBase34 instanceof ProdutoPerecivel34){
+//                    ProdutoPerecivel34 produtoPerecivel34 = (ProdutoPerecivel34) produtoBase34;
+//                    extra = String.format("Data Validade"+produtoPerecivel34.getDataValidade().format(formatter));
+//                }
+//                if (produtoBase34 instanceof ProdutosNaoPereciveis34){
+//                    ProdutosNaoPereciveis34 produtosNaoPereciveis34 = (ProdutosNaoPereciveis34) produtoBase34;
+//                    extra = String.format("Meses Garantia:"+produtosNaoPereciveis34.getMesesGarantia()
+//                    +" |Categoria:"+produtosNaoPereciveis34.getCategoria());
+//                }
+//                String linha = String.format("\"%s\",\"%.2f\",\"%d\",\"%d\",\"%s\"",produtoBase34.getNome(),produtoBase34.getPreco(),
+//                        produtoBase34.getQuantidade(),produtoBase34.getEstoqueMinimo(),extra);
+//                conteudo.append(linha).append("\n");
+//            }
+//            Files.writeString(caminhoArquivo,conteudo,StandardCharsets.UTF_8,StandardOpenOption.CREATE,
+//                    StandardOpenOption.TRUNCATE_EXISTING);
+//            System.out.println("Relatório gerado com sucesso:"+caminhoArquivo.toAbsolutePath());
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+//    }
+//
+
 
 //    public void gerarRelatorio2(List<ProdutoBase34>  produtoBase34s){
 //        Path pastaArquivo = Paths.get("relatorio");
