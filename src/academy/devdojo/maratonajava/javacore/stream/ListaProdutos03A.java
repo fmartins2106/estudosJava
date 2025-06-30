e992be746dde727438052c712db9afa0196e4ae1
Fernando Martins <fernandom.adm@gmail.com>
2025-06-30 09:17:19 -0300

    Treinando stream com grouping by

--- src/academy/devdojo/maratonajava/javacore/stream/ListaProdutos03A.java
+++ src/academy/devdojo/maratonajava/javacore/stream/ListaProdutos03A.java
@@ -3,22 +3,19 @@ package academy.devdojo.maratonajava.javacore.stream;
 import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Produto;
 import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Produto19;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ListaProdutos03A {
     private static List<ProdutoStream03> produtosLoja = new ArrayList<>(List.of(
-            new ProdutoStream03("Chaleira Eletrica",100,120.23,Categoria03.ELETRONICO),
             new ProdutoStream03("Arroz branco 1kg",222,2.32,Categoria03.ALIMENTO),
+            new ProdutoStream03("Chaleira Eletrica",100,120.23,Categoria03.ELETRODOMESTICO),
             new ProdutoStream03("Feijão Carioca 1kg",222,5.32,Categoria03.ALIMENTO),
-            new ProdutoStream03("Trigo 1kg",222,3.32,Categoria03.ALIMENTO),
             new ProdutoStream03("Iphone",442,3.455,Categoria03.ELETRONICO),
+            new ProdutoStream03("Trigo 1kg",222,3.32,Categoria03.ALIMENTO),
             new ProdutoStream03("Camisa branca basica",522,60.32,Categoria03.VESTUARIO),
-            new ProdutoStream03("Calça Jeans Masculina",2222,90.32,Categoria03.VESTUARIO),
             new ProdutoStream03("Liquidificador",322,122.52,Categoria03.ELETRODOMESTICO),
+            new ProdutoStream03("Calça Jeans Masculina",2222,90.32,Categoria03.VESTUARIO),
             new ProdutoStream03("Macarrão 500gr",222,2.12,Categoria03.ALIMENTO)
     ));
 
@@ -33,6 +30,35 @@ public class ListaProdutos03A {
         return produtosPorCategoria;// retorna o map com os produtos agrupados
     }
 
+
+//    public static Map<Categoria03,List<ProdutoStream03>> agruparPorCategoria(){
+//        Map<Categoria03,List<ProdutoStream03>> produtosAgrupadosCategoria = new HashMap<>();
+//        for (ProdutoStream03 produtoStream03 : produtosLoja) {
+//            produtosAgrupadosCategoria.computeIfAbsent(produtoStream03.getCategoria03(),k-> new ArrayList<>())
+//                    .add(produtoStream03);
+//        }
+//        return produtosAgrupadosCategoria;
+//    }
+//
+//    public static void exibirInfoProdutosCategoria(Map<Categoria03,List<ProdutoStream03>> agrupados){
+//        for (Map.Entry<Categoria03,List<ProdutoStream03>> entry : agrupados.entrySet()){
+//            System.out.println("Categoria:"+entry.getKey());
+//            for (ProdutoStream03 produtoStream03 : produtosLoja) {
+//                System.out.println("Produto:"+produtoStream03.getNome());
+//            }
+//        }
+//    }
+//
+//    public static void processarAgrupamentoCategoria(){
+//        Map<Categoria03,List<ProdutoStream03>> agrupados = agruparPorCategoria();
+//        exibirInfoProdutosCategoria(agrupados);
+//    }
+//
+//    public static Map<Categoria03,List<ProdutoStream03>> produtosAgrupadosCategoria(){
+//        return produtosLoja.stream().collect(Collectors.groupingBy(ProdutoStream03::getCategoria03));
+//    }
+
+
     // Metodo que imprime os produtos agruapdos por categoria
     public static void exibirProdutosAgrupados(Map<Categoria03,List<ProdutoStream03>> agrupados){
         for (Map.Entry<Categoria03,List<ProdutoStream03>> entry : agrupados.entrySet()){ // percorre cada categoria no map
@@ -44,18 +70,19 @@ public class ListaProdutos03A {
         }
     }
 
+    //    processar o agrupamento
     public static void processarAgrupamento(){
         Map<Categoria03,List<ProdutoStream03>> agrupados = agruparPorCategoria();  // chama o método de agrupamento
         exibirProdutosAgrupados(agrupados);  // exibe os produtos agrupados
     }
-
-
-
 
     // Versão alternativa usando Stream (estilo profissional)
     public static Map<Categoria03, List<ProdutoStream03>> AgruparPorCategoriaComStream() {
         return produtosLoja.stream()
                 .collect(Collectors.groupingBy(ProdutoStream03::getCategoria03));
     }
+
+
+
 
 }
