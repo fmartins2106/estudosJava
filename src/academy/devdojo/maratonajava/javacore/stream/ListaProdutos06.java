742cbc59af1781a129520ee0663f12c1f3e59df0
Fernando Martins <fernandom.adm@gmail.com>
2025-07-09 14:18:51 -0300

    Treinando gruping by exercicio ListaProduts06

---
+++ src/academy/devdojo/maratonajava/javacore/stream/ListaProdutos06.java
@@ -0,0 +1,73 @@
+package academy.devdojo.maratonajava.javacore.stream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ListaProdutos06 {
+    private static List<ProdutoStream03> produtosLoja = new ArrayList<>(List.of(
+            new ProdutoStream03("Chaleira Eletrica", 100, 120.23, Categoria03.ELETRONICO),
+            new ProdutoStream03("Arroz branco 1kg", 222, 2.32, Categoria03.ALIMENTO),
+            new ProdutoStream03("Feijão Carioca 1kg", 222, 5.32, Categoria03.ALIMENTO),
+            new ProdutoStream03("Trigo 1kg", 222, 3.32, Categoria03.ALIMENTO),
+            new ProdutoStream03("Iphone", 442, 3.455, Categoria03.ELETRONICO),
+            new ProdutoStream03("Camisa branca basica", 522, 60.32, Categoria03.VESTUARIO),
+            new ProdutoStream03("Calça Jeans Masculina", 2222, 90.32, Categoria03.VESTUARIO),
+            new ProdutoStream03("Liquidificador", 322, 122.52, Categoria03.ELETRODOMESTICO),
+            new ProdutoStream03("Macarrão 500gr", 222, 2.12, Categoria03.ALIMENTO)
+    ));
+
+    public static Promocao03 getPromocao(ProdutoStream03 produtoStream03){
+        return produtoStream03.getPreco() < 5 ? Promocao03.PROMOCAO : Promocao03.PRECO_NORMAL;
+    }
+
+    public static Map<Categoria03,List<ProdutoStream03>> agruparPorCategoria(List<ProdutoStream03> produto){
+        return produto.stream().collect(Collectors.groupingBy(ProdutoStream03::getCategoria03));
+    }
+
+    public static Map<Categoria03,Map<Promocao03,List<ProdutoStream03>>> agruparPorCategoriaEPromocao(List<ProdutoStream03> produtos){
+        return produtos.stream().collect(Collectors.groupingBy(ProdutoStream03::getCategoria03
+        ,Collectors.groupingBy(produtoStream03 -> produtoStream03.getPreco() < 5 ?
+                        Promocao03.PROMOCAO : Promocao03.PRECO_NORMAL)));
+    }
+
+    public static Map<Promocao03,List<ProdutoStream03>> agruparPorPromocao(List<ProdutoStream03> produtos){
+        return produtos.stream()
+                .collect(Collectors
+                        .groupingBy(produtoStream03
+                           -> produtoStream03.getPreco() < 5 ? Promocao03.PROMOCAO : Promocao03.PRECO_NORMAL));
+    }
+
+    public static Map<Categoria03,Long> contarProdutosPorCategoria(List<ProdutoStream03> produtos){
+        return produtos.stream().collect(Collectors.groupingBy(ProdutoStream03::getCategoria03,
+                Collectors.counting()));
+    }
+
+
+    public static Map<Categoria03, Optional<ProdutoStream03>> produtoMaisCaroCategoria(List<ProdutoStream03> produtos){
+        return produtos.stream()
+                .collect(Collectors.groupingBy(ProdutoStream03::getCategoria03
+                ,Collectors.maxBy(Comparator.comparing(ProdutoStream03::getPreco))));
+    }
+
+    public static Map<Categoria03,DoubleSummaryStatistics> mostrarEstatisticas(List<ProdutoStream03> produtos){
+        return produtos.stream()
+                .collect(Collectors.groupingBy(ProdutoStream03::getCategoria03
+                ,Collectors.summarizingDouble(ProdutoStream03::getPreco)));
+    }
+
+    public static Map<Categoria03,Map<Promocao03,List<ProdutoStream03>>> agruparMetodoAuxiliar(List<ProdutoStream03> produtos){
+        return produtos.stream()
+                .collect(Collectors.groupingBy(ProdutoStream03::getCategoria03
+                ,Collectors.groupingBy(ListaProdutos06::getPromocao)));
+    }
+
+    public static void main(String[] args) {
+       agruparPorCategoria(produtosLoja).forEach((categoria03, produtoStream03s)
+               -> {
+           System.out.println("Categoria:"+categoria03+" |Produto:"+produtoStream03s);
+       });
+    }
+
+
+
+}
