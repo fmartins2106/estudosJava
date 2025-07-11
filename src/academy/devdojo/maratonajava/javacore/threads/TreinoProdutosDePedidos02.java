b138577a56f5bbeccf2458bee19ba618817b8775
Fernando Martins <fernandom.adm@gmail.com>
2025-07-11 09:49:09 -0300

    Treinando ProdutorDePedidos02

--- src/academy/devdojo/maratonajava/javacore/threads/TreinoProdutosDePedidos02.java
+++ src/academy/devdojo/maratonajava/javacore/threads/TreinoProdutosDePedidos02.java
@@ -1,23 +1,20 @@
 package academy.devdojo.maratonajava.javacore.threads;
 
-import academy.devdojo.maratonajava.javacore.threads.dominio.Pedido04;
 import academy.devdojo.maratonajava.javacore.threads.dominio.Pedido05;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 
-public class ProdutorDePedidos implements Runnable{
-    public final BlockingQueue<Double> filaPedido;
+public class TreinoProdutosDePedidos02 implements Runnable{
+    public final BlockingQueue<Pedido05> filaPedido;
     public final List<String> clientes = Arrays.asList("Ana","Maria","Joana","Fernando","Daisy","Marta","Manuel");
     public Random random = new Random();
 
-
-    public ProdutorDePedidos(BlockingQueue<Double> filaPedido) {
+    public TreinoProdutosDePedidos02(BlockingQueue<Pedido05> filaPedido) {
         this.filaPedido = filaPedido;
     }
 
@@ -25,14 +22,14 @@ public class ProdutorDePedidos implements Runnable{
         while (!Thread.currentThread().isInterrupted()){
             try {
                 String cliente = clientes.get(random.nextInt(clientes.size()));
-                double valorBruto = 50 + (random.nextInt(450));
-                double valor = new BigDecimal(valorBruto).setScale(2,RoundingMode.HALF_UP)
+                double valorBruto = 50 + random.nextDouble(450);
+                double valor = new BigDecimal(valorBruto).setScale(2, RoundingMode.HALF_UP)
                         .doubleValue();
                 Pedido05 pedido05 = new Pedido05(cliente,valor);
-                System.out.println("Novo Pedido adicionado");
+                System.out.println("Novo pedido adicionado.");
                 Thread.sleep(1000 + random.nextInt(2000));
             }catch (InterruptedException e){
-                System.out.println("Produtor interrompido.");
+                System.out.println("Erro no produtor.");
                 Thread.currentThread().interrupt();
             }catch (Exception e){
                 System.out.println("Erro inesperado no produtor:"+e.getMessage());
